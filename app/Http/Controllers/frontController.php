37a4b419dbcc3c9967f51698edd495d2ab3dfb72
andjica <33286611+andjica@users.noreply.github.com>
2018-07-08 01:14:19 +0200

    adding new pages

--- app/Http/Controllers/frontController.php
+++ app/Http/Controllers/frontController.php
@@ -10,4 +10,23 @@ class frontController extends Controller
 
         return view('pages.home');
     }
+
+    public function get_dev(){
+
+        return view('pages.development');
+    }
+    public function get_rec(){
+
+        return view('pages.recruitment');
+    }
+    public function get_ite(){
+
+        return view('pages.items');
+    }
+    public function get_acctypes(){
+
+        return view('pages.accounttypes');
+    }
+    
+
 }
