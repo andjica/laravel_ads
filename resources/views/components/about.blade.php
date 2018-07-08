752a51b5bf93cb05ebdb082289309c8995cbe54e
andjica <33286611+andjica@users.noreply.github.com>
2018-07-08 15:36:05 +0200

    Adding new text for about

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -3,8 +3,13 @@
       <div class="container text-center">
         <div class="row">
           <div class="col-lg-10 mx-auto">
-            <h2>Stylish Portfolio is the perfect theme for your next project!</h2>
-            <p class="lead mb-5">This theme features a flexible, UX friendly sidebar menu and stock photos from our friends at
+            <h2>About IederWatWils</h2>
+            <p class="lead mb-5 text-danger">
+            IederWatWils
+          offering placement for multiple categories for 1 ad, ElkeVatVils can be great
+          profitable acquisition on the market. With free version, you place two ads, 20 images, google maps .. With PRO and Supper-PRO you can view the footage as well as links to your websites or other ..
+
+    IeserWatWils offers super marketing to your ad! Sign up and advertise!
               <a href="https://unsplash.com/">Unsplash</a>!</p>
             <a class="btn btn-dark btn-xl js-scroll-trigger" href="#services">What We Offer</a>
           </div>
