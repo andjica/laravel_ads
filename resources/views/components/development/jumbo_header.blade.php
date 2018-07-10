b2bbe596b7cca0e6969a2f5f76c1ed2215e8f5ec
andjica <33286611+andjica@users.noreply.github.com>
2018-07-10 18:32:24 +0200

    mod

--- resources/views/components/development/jumbo_header.blade.php
+++ resources/views/components/development/jumbo_header.blade.php
@@ -1,11 +1,13 @@
 
 
 
-
+@if(auth()->check())
+    <div class="row">
+    @include('components.development.sidebar')
     <div class="col-lg-9">
 
     <header class="jumbotron my-4">
-        <h1 class="display-4 text-secondary">Weclome to Development ADS</h1>
+        <h1 class="display-4 text-info">Weclome to Development ADS</h1>
         <img src="./img/dev1.jpeg" class="img-fluid">
         <p class="lead">Here you can post your ads and modified it!</p>
         <a href="#" class="btn btn-primary btn-lg">Call to action!</a>
@@ -15,15 +17,11 @@
 
 
 </div>
+@else
+<header class="jumbotron my-4">
+        <h1 class="display-4 text-secondary">Welcome to Development ADS</h1>
         <img src="./img/dev1.jpeg" class="img-fluid">
-        <p class="lead">
-           We've prepared your ads on this page. Ads for web developers, new technologies,
-           mobile apps, software and other global applications on the market. 
-           Here you can show yourads that will include a lot of text and video.
-          
-          <b class="text-info">If you still do not have your profile, make it quick and easy:</b>
-        </p>
-     
+        <p class="lead">Here you can post your ads and modified it!</p>
         <a href="#" class="btn btn-primary btn-lg">Call to action!</a>
       </header>
 @endif
