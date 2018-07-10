2f1bf4405dfcf76f16aa94567433ab108c70a0d3
andjica <33286611+andjica@users.noreply.github.com>
2018-07-10 17:00:27 +0200

    Komentar obavezan-za sesiju

--- resources/views/pages/recruitment.blade.php
+++ resources/views/pages/recruitment.blade.php
@@ -6,8 +6,11 @@
     <link href="{{asset('/')}}css/blog-home.css" rel="stylesheet">
 @endsection
 @section('top')
-@include('components.ads.sign_in')
+
     @include('components.ads.nav_ads')
+    <!--Postaviti sesiju, ako sesija postoji skloniti div sign_in -->
+    @include('components.ads.sign_in')
+    
 @endsection
 @section('content')
 <div class="container">
