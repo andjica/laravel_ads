d45ae7e9ce55325e818345e3ed5be788c8dcf370
alokin95 <nikola_m95@hotmail.com>
2018-07-10 18:02:47 +0200

    Merge request

--- resources/views/components/development/jumbo_header.blade.php
+++ resources/views/components/development/jumbo_header.blade.php
@@ -1,14 +1,10 @@
 
 
-<!-- Jumbotron Header -->
 
-    <!--ako sesija postoji koristicemo ova dva diva,
-    sidebar i header jumbo -->
-
-    <!--<div class="row">
+@if(auth()->check())
+    <div class="row">
     @include('components.development.sidebar')
     <div class="col-lg-9">
-     Jumbotron Header 
 
     <header class="jumbotron my-4">
         <h1 class="display-4 text-secondary">Weclome to Development ADS</h1>
@@ -21,10 +17,13 @@
 
 
 </div>
--->
+@else
 <header class="jumbotron my-4">
-        <h1 class="display-4 text-secondary">Weclome to Development ADS</h1>
+        <h1 class="display-4 text-secondary">Welcome to Development ADS</h1>
         <img src="./img/dev1.jpeg" class="img-fluid">
         <p class="lead">Here you can post your ads and modified it!</p>
         <a href="#" class="btn btn-primary btn-lg">Call to action!</a>
       </header>
+@endif
+
+
