f8ab31e06d04813709e89c1d800d8c3fe028b9e6
andjica <33286611+andjica@users.noreply.github.com>
2018-07-10 23:21:16 +0200

    Modified

--- resources/views/components/user/top_info.blade.php
+++ resources/views/components/user/top_info.blade.php
@@ -1,8 +1,8 @@
-<section class="p-1 bg-dark text-muted text-center" id="sign">
+<section class="p-2 bg-dark text-muted text-center" id="sign">
 
       <div class="container text-center">
-        <h3 class="mb-4">Sign in to start your ads...</h3>
-        <p>Lorem ipsum dolor sit amet, duis dolorum vis at. 
+        <h4 class="mb-4">Welcome to User Panel...</h4>
+        <p class="lead">Lorem ipsum dolor sit amet, duis dolorum vis at. 
           Sea ad ubique putant legimus, mel petentium consetetur no, 
           verterem deterruisset cu his. Nec suscipit deseruisse ne, 
           has te mundi libris dignissim, sumo brute zril ei his. 
@@ -11,7 +11,8 @@
         <i class="fa fa-code text-info" style="font-size:48px;"></i>
         <i class="fa fa-thumbs-o-up text-info" style="font-size:48px;"></i>
         <i class="fa fa-heart text-info" style="font-size:48px;"></i>
-        <a href="{{asset('//accounttypes')}}" class="text-light">Make ad with 25 photos!</a>
+ 
+        <a href="{{asset('/accounttypes')}}" class="text-light badge badge-danger" data-toggle="tooltip" data-placement="top" title="See PRO-packet">Make AD with 25 photos!</a>
 			</div>
 			<hr>
     </section>
