2c2160b10def87ed5520302eb9b98f3b7ecf5eb3
andjica <33286611+andjica@users.noreply.github.com>
2018-07-21 01:42:15 +0200

    promena

--- resources/views/components/ads/sign_in.blade.php
+++ resources/views/components/ads/sign_in.blade.php
@@ -1,7 +1,7 @@
 <section class="p-1 bg-muted text-muted text-center" id="sign">
 <img src="./img/logo.png" class="img-fluid" style="width:50%; height:50%;"/>
       <div class="container text-center">
-        <h3 class="mb-4">Sign in to start your ads...</h3>
+        <h3 class="mb-4">Start uw eerste advertentie...</h3>
 			<form class="form-inline justify-content-center" method="POST" action="{{asset('/login')}}">
 				@csrf
 				<div class="form-group mx-sm-3 mb-2">
--- resources/views/components/items/jumbo_header.blade.php
+++ resources/views/components/items/jumbo_header.blade.php
@@ -13,9 +13,8 @@
         <img src="./img/sec1.jpeg" class="img-fluid" alt="img recruitment, iederwatwils">
         <p class="lead text-white p-3 bg-secondary rounded my-3">
 
-             Laat de apparatuur die u niet gebruikt op uw telefoon zien aan kleding, 
-          schoenen, visuitrusting, fietsen, oude rackets, horloges, accessoires .. EveriVatVils beperkt u niet en geeft u de mogelijkheid om alles 
-          wat u wilt van mobiele telefoons toe te voegen aan kleding die u niet draagt. 
+           U bent uw spulletjes zat, maar gunt het een 2e kans?, u hebt teveel en wilt iets minder?
+Via IEDERWATWILS profiteert u van de beste winst, en maakt u mooie mensen blij met uw eerdere aankoop. Een mooie win win situatie! 
             <b class="text-white">Registreer en meld je aan:</b>       
  
    
