cec2c5109ee272cb324c15be7cb36e972e13eaa3
andjica <33286611+andjica@users.noreply.github.com>
2018-07-24 14:49:09 +0200

    tekst menjan sad cemo ostalo

--- resources/views/components/company/jumbo_header.blade.php
+++ resources/views/components/company/jumbo_header.blade.php
@@ -7,16 +7,17 @@
     <div class="col-lg-9">
 
    <header class="jumbotron my-4 m-4 bg-white">
-        <h1 class="display-4 text-info"> IederWatWils - Voertuigen Advertentie</h1>
+        <h1 class="display-4 text-info"> IederWatWils - Bedrijf promotie</h1>
         <a class="btn btn-danger btn-sm mb-3 js-scroll-trigger text-white" href="#ads">Find Out More</a>
         <br>
         <img src="./img/comp.jpeg" class="img-fluid" alt="img autos, iederwatwils">
         <p class="lead text-white p-3 bg-danger rounded my-3">
-        Auto, Motor, Scooter, Camper, Tractor, of wat dan ook, met een mooie
-        uitgebreide advertentie bij <b>IEDERWATWILS</b> verkoopt u uw voertuig supersnel en
-        met de meeste winst. Laat vee1 mooie fotoʼs, en een zelfgemaakte video zien dat verkoopt! 
+          Kan uw bedrijf een boost gebruiken, of heeft uw bedrijf extra mankracht nodig?
+          <b>IederWatWils</b> kan het voor u leveren. Maak een doeltreffende en aantrekkelijke advertentie
+          via IederWatWils met de mooiste foto's en zalfgemaakte video's en uw bedrijf is binnen no time weer waar het 
+          geweest is, en zelfs verder!!
         <b class="text-white"> 
-            Heeft u nog geen account?? Registreer dan snel en gratis
+            Heeft u nog geen account?? Registreer dan snel en gratis:
           </b>
             </p>
         <a href="{{asset('/register')}}" class="btn btn-primary btn-lg">Sign up</a>
@@ -35,11 +36,12 @@
         <br>
         <img src="./img/comp.jpeg" class="img-fluid" alt="img autos, iederwatwils">
         <p class="lead text-white p-3 bg-danger rounded my-3">
-        Auto, Motor, Scooter, Camper, Tractor, of wat dan ook, met een mooie
-        uitgebreide advertentie bij <b>IEDERWATWILS</b> verkoopt u uw voertuig supersnel en
-        met de meeste winst. Laat vee1 mooie fotoʼs, en een zelfgemaakte video zien dat verkoopt! 
+          Kan uw bedrijf een boost gebruiken, of heeft uw bedrijf extra mankracht nodig?
+          <b>IederWatWils</b> kan het voor u leveren. Maak een doeltreffende en aantrekkelijke advertentie
+          via IederWatWils met de mooiste foto's en zalfgemaakte video's en uw bedrijf is binnen no time weer waar het 
+          geweest is, en zelfs verder!!
         <b class="text-white"> 
-            Heeft u nog geen account?? Registreer dan snel en gratis
+            Heeft u nog geen account?? Registreer dan snel en gratis:
           </b>
             </p>
         <a href="{{asset('/register')}}" class="btn btn-primary btn-lg">Sign up</a>
