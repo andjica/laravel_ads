6f0dfac40c5f4f02f007129fe258111c00b2986b
andjica <33286611+andjica@users.noreply.github.com>
2018-07-27 18:26:29 +0200

    modified

--- resources/views/components/user/info.blade.php
+++ resources/views/components/user/info.blade.php
@@ -8,20 +8,15 @@
     @endif
 
     @if ($message = Session::get('success'))
-    <div class="w3-panel w3-green w3-display-container">
-        <span onclick="this.parentElement.style.display='none'"
-        class="w3-button w3-green w3-large w3-display-topright">&times;</span>
-        <p>{!! $message !!}</p>
-    </div>
+   
+    
+        <p class="alert alert-success">{!! $message !!}</p>
+
     <?php Session::forget('success');?>
     @endif
 
     @if ($message = Session::get('error'))
-    <div class="w3-panel w3-red w3-display-container">
-        <span onclick="this.parentElement.style.display='none'"
-        class="w3-button w3-red w3-large w3-display-topright">&times;</span>
-        <p>{!! $message !!}</p>
-    </div>
+    <p class="alert alert-success">{!! $message !!}</p>
     <?php Session::forget('error');?>
     @endif
     
