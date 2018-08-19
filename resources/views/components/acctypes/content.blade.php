e79eae43f922937f9ee562b782ef8cd500125f7a
Nikola <nikola_m95@hotmail.com>
2018-08-19 15:40:56 +0200

    changed text

--- resources/views/components/acctypes/content.blade.php
+++ resources/views/components/acctypes/content.blade.php
@@ -1,16 +1,18 @@
 
 
-
-
 <div class="col-md-8">
 <img src="{{asset('/img/logo.png')}}" class="img-fluid"/>
 <h3 class="my-2 text-danger">Heeft voor jou een gratis advertentie voor je klaargemaakt
 </h3>
-  <p class="text-info">Iederwatwils heeft een speciale aanbieding voor u voorbereid, maakt uw advertentie en stelt deze in. Door je profiel te maken kun je een van de drie pakketten kiezen. Met een gratis pakket maakt u één advertentie die tot één categorie behoort, vijf foto's, uw eigen map, titel, inhoud en andere gegevens. 
-    Zie onze pakkettabel voor andere pakketten.</p>
+  <p class="text-info">Iederwatwils heeft een speciale aanbieding voor 
+  u voorbereid, maakt uw advertentie en geef uw voorkeur op. 
+  Door een eigen account aan te maken kan je kostenloos een eigen advertentie uitproberen met 
+  vijf foto's, uw eigen map, titel, inhoud en andere gegevens. 
+    Voor een upgrade van uw account bekijk hieronder de mogelijheden.</p>
     <a href="#table1" class="btn btn-info btn-sm">Zie pakketten</a>
-  <img class="img-fluid my-5" src="./img/service4.jpeg" alt="" >
+  <img class="img-fluid my-5" src="./img/service4.jpeg" alt="iederwatwils pakketten">
   
   @include('components.acctypes.table')
 </div>
+
 
--- resources/views/components/acctypes/list.blade.php
+++ resources/views/components/acctypes/list.blade.php
@@ -24,34 +24,34 @@
   </div>
   <img src="./img/acc_2.jpeg" class="img-fluid"/>
   <p class="mb-1">
-    Op zoek naar een baan? Maak je eigen werk, indrukken staan ​​hier tussenin.
+    Op zoek naar een baan? Plaats hier uw eigen portfolio.
   </p>
   <small class="text-muted">IederWatWils 2018.</small>
 </a>
 <a href="{{asset('/items')}}" class="list-group-item list-group-item-action flex-column align-items-start">
   <div class="d-flex w-100 justify-content-between">
     <h5 class="mb-1">Tweede hands</h5>
-    <small class="text-muted">3 days ago</small>
+    <small class="text-muted">3 dagen geleden</small>
   </div>
   <img src="./img/acc_3.jpeg" class="img-fluid"/>
   <p class="mb-1">
-    Als je te veel kleding hebt, accesorizes, horloges en meer, verkoop dan via IederWatWils.
+    Als je kleding hebt, accessoires, horloges en andere artikelen, verkoop dat dan via IederWatWils.
   </p>
   <small class="text-muted">IederWatWils 2018</small>
 </a>
 <a href="{{asset('/cars')}}" class="list-group-item list-group-item-action flex-column align-items-start">
   <div class="d-flex w-100 justify-content-between">
     <h5 class="mb-1">Auto's</h5>
-    <small class="text-muted">3 days ago</small>
+    <small class="text-muted">3 dagen geleden</small>
   </div>
   <img src="./img/acc_4.jpeg" class="img-fluid"/>
-  <p class="mb-1">Adverteer bij IederWatWils welke voertuigen in wilt verkopen.</p>
+  <p class="mb-1">Adverteer bij IederWatWils welke voertuigen u wilt verkopen.</p>
   <small class="text-muted">IederWatWils 2018.</small>
 </a>
 <a href="{{asset('/houses')}}" class="list-group-item list-group-item-action flex-column align-items-start">
   <div class="d-flex w-100 justify-content-between">
     <h5 class="mb-1">Huizen</h5>
-    <small class="text-muted">3 days ago</small>
+    <small class="text-muted">3 dagen geleden</small>
    </div>
   <img src="./img/acc_5.jpeg" class="img-fluid"/>
   <p class="mb-1">Op zoek naar een ideaal huis? Zie IederWatWils eigen advertenties.</p>
